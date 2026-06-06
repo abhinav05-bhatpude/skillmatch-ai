7c05721455249cea2536d0d61728037ffa424564
abhinav05-bhatpude <abhinavbhatpude@gmail.com>
2026-06-06 14:55:50 -0700

    feat(models): create resume schema

---
+++ server/src/models/Resume.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+const resumeSchema = new mongoose.Schema(
+  {
+    fileName: {
+      type: String,
+      required: true,
+    },
+
+    extractedText: {
+      type: String,
+      default: "",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model(
+  "Resume",
+  resumeSchema
+);
