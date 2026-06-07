310a88162afa478ca234e41165dc3c6a5fdeab47
abhinav05-bhatpude <abhinavbhatpude@gmail.com>
2026-06-07 10:20:43 -0700

    feat(app): register resume routes

--- server/src/app.js
+++ server/src/app.js
@@ -2,11 +2,13 @@ const express=require("express");
 const cors=require("cors");
 const healthRoutes=require("./routes/healthRoutes");
 const internshipRoutes=require("./routes/internshipRoutes");
+const resumeRoutes = require("./routes/resumeRoutes");
 
 const app=express();
 
 app.use("/api/health",healthRoutes);
 app.use("/api/internships",internshipRoutes);
+app.use("/api/resumes",resumeRoutes);
 app.use(cors());
 app.use(express.json());
 
