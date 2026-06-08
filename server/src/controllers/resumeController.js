e937619f334be8c5130dcefd812b2954c254f6f8
abhinav05-bhatpude <abhinavbhatpude@gmail.com>
2026-06-08 11:55:44 -0700

    feat(controller): integrate pdf parser into upload flow

--- server/src/controllers/resumeController.js
+++ server/src/controllers/resumeController.js
@@ -1,66 +1,51 @@
 const Resume = require("../models/Resume");
+const extractedPdfText = require("../services/pdfService");
 
-const uploadResume = async (
-  req,
-  res
-) => {
+const uploadResume = async (req, res) => {
   try {
+    const resume = await Resume.create({
+      fileName: req.file.filename,
 
-    const resume =
-      await Resume.create({
-        fileName: req.file.filename,
-      });
+      extractedText: await extractPdfText(req.file.path),
+    });
 
     res.status(201).json({
       success: true,
       resume,
     });
-
   } catch (error) {
-
     res.status(500).json({
       message: error.message,
     });
-
   }
 };
 
-const getResumes = async (req,res) => {
-    try{
-        const resumes = await Resume.find();
+const getResumes = async (req, res) => {
+  try {
+    const resumes = await Resume.find();
 
-        res.status(200).json(resumes);
-    } catch(error){
-        res.status(500).json({
-            message:error.message,
-        });
-    }
+    res.status(200).json(resumes);
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
 };
 
-const deleteResume = async (
-  req,
-  res
-) => {
+const deleteResume = async (req, res) => {
   try {
-
-    await Resume.findByIdAndDelete(
-      req.params.id
-    );
+    await Resume.findByIdAndDelete(req.params.id);
 
     res.status(200).json({
       success: true,
       message: "Resume Deleted",
     });
-
   } catch (error) {
-
     res.status(500).json({
       message: error.message,
     });
-
   }
 };
-
 
 module.exports = {
   uploadResume,
