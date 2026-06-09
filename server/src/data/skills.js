b876fb797e2605445338974fc40f33a031e79fce
abhinav05-bhatpude <abhinavbhatpude@gmail.com>
2026-06-09 12:55:58 -0700

    feat(ai): create skills database

---
+++ server/src/data/skills.js
@@ -0,0 +1,19 @@
+const skills = [
+  "React",
+  "Node.js",
+  "Express",
+  "MongoDB",
+  "JavaScript",
+  "TypeScript",
+  "HTML",
+  "CSS",
+  "Git",
+  "GitHub",
+  "SQL",
+  "Python",
+  "Java",
+  "C",
+  "C++",
+];
+
+module.exports = skills;
