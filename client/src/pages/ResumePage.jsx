5bb4e236fbe269b227dbd28b27103f31e658b98b
abhinav05-bhatpude <abhinavbhatpude@gmail.com>
2026-06-10 14:06:39 -0700

    feat(ui): integrate upload section

--- client/src/pages/ResumePage.jsx
+++ client/src/pages/ResumePage.jsx
@@ -1,11 +1,17 @@
-function ResumePage(){
-    return(
-        <div className="p-8">
-            <h1 className="text-3xl font-bold">
-                Upload Resume
-            </h1>
-        </div>
-    );
+import ResumeUpload from "../components/ResumeUpload";
+
+function ResumePage() {
+  return (
+    <div className="max-w-4xl mx-auto p-8">
+
+      <h1 className="text-4xl font-bold mb-6">
+        Upload Resume
+      </h1>
+
+      <ResumeUpload />
+
+    </div>
+  );
 }
 
 export default ResumePage;
