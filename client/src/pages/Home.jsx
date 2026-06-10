e8ea577d9b1aef798550168ad3fa0b1afbedb0d4
abhinav05-bhatpude <abhinavbhatpude@gmail.com>
2026-06-10 14:02:13 -0700

    feat(ui): integrate features into home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import Hero from "../components/Hero";
+import Features from "../components/Features";
 
 function Home() {
   return (
-    <div>
+    <>
       <Hero />
-    </div>
+      <Features />
+    </>
   );
 }
 
