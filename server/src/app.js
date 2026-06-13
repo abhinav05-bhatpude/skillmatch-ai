561f4ad65a9040a2a3ac74a1de9333b9e9e6a368
abhinav05-bhatpude <abhinavbhatpude@gmail.com>
2026-06-13 14:09:54 -0700

    fix(cors):move cors middleware before routes

--- server/src/app.js
+++ server/src/app.js
@@ -1,22 +1,55 @@
-const express=require("express");
-const cors=require("cors");
-const healthRoutes=require("./routes/healthRoutes");
-const internshipRoutes=require("./routes/internshipRoutes");
-const resumeRoutes = require("./routes/resumeRoutes");
+const express = require("express");
+const cors = require("cors");
 
-const app=express();
+const healthRoutes =
+require("./routes/healthRoutes");
 
-app.use("/api/health",healthRoutes);
-app.use("/api/internships",internshipRoutes);
-app.use("/api/resumes",resumeRoutes);
-app.use(cors());
+const internshipRoutes =
+require("./routes/internshipRoutes");
+
+const resumeRoutes =
+require("./routes/resumeRoutes");
+
+const app = express();
+
+/*
+   IMPORTANT:
+   CORS must come BEFORE routes
+*/
+
+app.use(
+  cors({
+    origin:
+    "http://localhost:5173",
+    credentials: true,
+  })
+);
+
 app.use(express.json());
 
-app.get("/",(req,res)=>{
-    res.status(200).json({
-        success:true,
-        message:"SkillMatch AI API Running"
-    });
+app.use(
+  "/api/health",
+  healthRoutes
+);
+
+app.use(
+  "/api/internships",
+  internshipRoutes
+);
+
+app.use(
+  "/api/resumes",
+  resumeRoutes
+);
+
+app.get("/", (req, res) => {
+
+  res.status(200).json({
+    success: true,
+    message:
+    "SkillMatch AI API Running",
+  });
+
 });
 
-module.exports=app;
+module.exports = app;
